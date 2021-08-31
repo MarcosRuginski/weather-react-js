fd5b1bc8acaadaf6e07b254b88bf9898e4d89dee
Marcos Roberto Ruginski <marcosrobertoruginski@gmail.com>
2021-08-30 22:48:02 -0300

    Adsense

--- pages/index.js
+++ pages/index.js
@@ -65,6 +65,11 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <script
+          data-ad-client="ca-pub-9954854028991061"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        />
       </Head>
 
       <Layout setData={setData} data={data}>
